a87f37c6fb827c0bf4b67d5a07a49036324148a9
Oswaldo Alonzo <oalonzo@gmail.com>
2021-11-07 11:03:57 -0600

    realizando pruebas con markdown

--- js/script.js
+++ js/script.js
@@ -1,22 +1,29 @@
 //* las variables del Dom se les antepone $
 
 const d = document,
-    $section = d.querySelector("section");
+    $post1 = d.querySelector("#post1");
+$post2 = d.querySelector("#post2");
 
 fetch("markdown/prueba.md")
     .then((res) => (res.ok ? res.text() : Promise.reject(res)))
     .then((text) => {
         //console.log(text);
-        $section.innerHTML = new showdown.Converter().makeHtml(text);
-    })
-fetch("markdown/prueba2.md")
-    .then((res) => (res.ok ? res.text() : Promise.reject(res)))
-    .then((text) => {
-        //console.log(text);
-        $section.innerHTML = new showdown.Converter().makeHtml(text);
+        $post1.innerHTML = new showdown.Converter().makeHtml(text);
     })
     .catch((err) => {
         console.log(err);
         let message = err.statusText || "Ocurrió un Error";
-        $section.innerHTML = `Error ${err.status}:${message}`;
+        $post1.innerHTML = `Error ${err.status}:${message}`;
+    });
+
+fetch("markdown/prueba2.md")
+    .then((res) => (res.ok ? res.text() : Promise.reject(res)))
+    .then((text) => {
+        //console.log(text);
+        $post2.innerHTML = new showdown.Converter().makeHtml(text);
+    })
+    .catch((err) => {
+        console.log(err);
+        let message = err.statusText || "Ocurrió un Error";
+        $post2.innerHTML = `Error ${err.status}:${message}`;
     });
